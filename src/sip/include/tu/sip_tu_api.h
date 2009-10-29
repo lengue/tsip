42f70e088c5176ab4831d50db780c0f7326f1e47
tommy1boy <tommy1boy@163.com>
2009-10-29 14:41:55 +0000

    将SIP同APP之间的调用机制改为消息接口

--- src/sip/include/tu/sip_tu_api.h
+++ src/sip/include/tu/sip_tu_api.h
@@ -3,7 +3,7 @@ extern ULONG SIP_TU_RecvUpMsg(ULONG ulCoreID,
                               SIP_LOCATION_S *pstPeerAddr,
                               UBUF_HEADER_S  *pstUbufSipMsg);
 extern ULONG SIP_TU_RecvDownMsg(ULONG ulAppRef1,
-                         ULONG ulAppRef2,
-                         ULONG *pulStackRef1,
-                         ULONG *pulStackRef2,
-                         UBUF_HEADER_S  *pstUbufSipMsg);
+                                ULONG ulAppRef2,
+                                ULONG ulStackRef1,
+                                ULONG ulStackRef2,
+                                UBUF_HEADER_S  *pstUbufSipMsg);
--- src/sip/tu/include/ua/sip_ua_api.h
+++ src/sip/tu/include/ua/sip_ua_api.h
@@ -1,10 +1,10 @@
 extern ULONG SIP_UA_RecvDownMsg(ULONG ulAppRef1,
                                 ULONG ulAppRef2,
-                                ULONG *pulStackRef1,
-                                ULONG *pulStackRef2,
+                                ULONG ulStackRef1,
+                                ULONG ulStackRef2,
                                 UBUF_HEADER_S *pstUbufSipMsg);
 extern ULONG SIP_UAS_SendResponse(IN  ULONG ulTxnID,
-                                  OUT ULONG *pulDlgID,
+                                  OUT ULONG ulDlgID,
                                   UBUF_HEADER_S * pstUbufSipMsg);
 extern ULONG SIP_UA_Init();
 extern ULONG SIP_UA_RecvUpMsg(ULONG ulCoreID,
