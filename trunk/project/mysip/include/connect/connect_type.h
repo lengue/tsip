211707f109d41e052fa969bbff2668a4080a55f5
tommy1boy@163.com <tommy1boy@163.com@6ea01b42-bff3-11de-8784-f7ad4773dbab>
2010-01-01 02:32:12 +0000

    事物匹配

--- trunk/project/mysip/include/connect/connect_type.h
+++ trunk/project/mysip/include/connect/connect_type.h
@@ -1,17 +1,17 @@
 /* IP地址和端口号必须是网络字节序的 */
 typedef struct tagCONN_LOCATION_S
 {
-    SIP_TRANSPORT_PROTOCOL_E eProtocol;
-    ULONG   ulIpAddr;/* IPV4 */
+    CONN_TRANSPORT_PROTOCOL_E eProtocol;
+    ULONG  ulIpAddr;
     USHORT usPort;
 }CONN_LOCATION_S;
 
-typedef ULONG (* CONN_FUNC_MSG_PROC)(SIP_LOCATION_S *pstLocation,
+typedef ULONG (* CONN_FUNC_MSG_PROC)(CONN_LOCATION_S *pstLocation,
                                      UCHAR *pucMsg,
                                      ULONG ulMsgLen);
 typedef struct tagCONN_CFG_S
 {
     UCHAR  ucListenNum;
-    CONN_LOCATION_S stListen[SIP_TXP_MAX_LISTEN_NUM];
+    CONN_LOCATION_S stListen[CONN_MAX_LISTEN_NUM];
     CONN_FUNC_MSG_PROC pfnMsgProc;
 }CONN_CFG_S;
