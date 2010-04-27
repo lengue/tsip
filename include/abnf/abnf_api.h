bc834584d6abb8cf043701c63ad33e9df88c140c
tommy1boy <tommy1boy@163.com>
2010-04-27 16:15:10 +0000

    ABNF优化

--- include/abnf/abnf_api.h
+++ include/abnf/abnf_api.h
@@ -7,11 +7,13 @@
 extern ULONG ABNF_Init();
 extern ULONG ABNF_BuildRuleList(UCHAR *pucText,
                                 ULONG  ulLen,
-                                ABNF_RULE_LIST_S **ppstRuleList);
-extern ULONG ABNF_FreeRuleList(ABNF_RULE_LIST_S *pstRuleList);
+                                void **ppstRuleList);
+extern ULONG ABNF_GetRuleIndex(void *pstRuleList, 
+                               UCHAR *pucRuleName, 
+                               ULONG *pulRuleIndex);
 extern ULONG ABNF_GrammarParse(UCHAR *pucText,
-                                ULONG  ulLen,
-                                ABNF_RULE_LIST_S *pstRuleList,
+                                ULONG ulLen,
+                                void *pstRuleList,
                                 ULONG ulMatchRule,
                                 ABNF_GRAMMAR_NODE_S **ppstNode);
 extern ULONG ABNF_FreeNodeTree(ABNF_GRAMMAR_NODE_S *pstGrammarNode);
